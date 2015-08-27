0ec79e9cec81134c028147e1f54bbf4eb17b0c62
Angus McLeod <angus@mcleod.org.au>
2015-08-27 11:14:46 -0400

    init KnoteUp iOS

---
+++ webapp/mobile-config.js
@@ -0,0 +1,27 @@
+App.info({
+  id: 'com.knotable.knoteup',
+  name: 'KnoteUp',
+  description: 'KnoteUp iOS',
+  version: '0.0.1',
+  author: 'Knotable',
+  email: 'team@knote.com',
+  website: 'http://knotable.com'
+});
+
+// Resources
+App.icons({
+  'iphone': 'public/icon.png',
+  // ... more screen sizes and platforms ...
+});
+
+App.launchScreens({
+  'iphone': 'public/launch.png',
+  // ... more screen sizes and platforms ...
+});
+
+// Cordova preferences
+App.setPreference('HideKeyboardFormAccessoryBar', true);
+App.setPreference('Orientation', 'portrait');
+
+// Accesss rules
+App.accessRule('*')
